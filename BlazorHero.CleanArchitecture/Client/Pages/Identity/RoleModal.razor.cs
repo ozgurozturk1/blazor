3a764732f82c48cecca2acc355cab26615b05ff8
ozgurozturk1 <60068731+ozgurozturk1@users.noreply.github.com>
2021-05-16 03:44:57 +0300

    Initial commit

---
+++ BlazorHero.CleanArchitecture/Client/Pages/Identity/RoleModal.razor.cs
@@ -0,0 +1,66 @@
+﻿using BlazorHero.CleanArchitecture.Application.Requests.Identity;
+using BlazorHero.CleanArchitecture.Client.Extensions;
+using BlazorHero.CleanArchitecture.Shared.Constants.Application;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using MudBlazor;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Client.Pages.Identity
+{
+    public partial class RoleModal
+    {
+        private bool success;
+        private string[] errors = { };
+        private MudForm form;
+
+        [Parameter]
+        public string Id { get; set; }
+
+        [Parameter]
+        [Required]
+        public string Name { get; set; }
+
+        [CascadingParameter] private MudDialogInstance MudDialog { get; set; }
+
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+
+        [CascadingParameter] public HubConnection hubConnection { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            hubConnection = hubConnection.TryInitialize(_navigationManager);
+            if (hubConnection.State == HubConnectionState.Disconnected)
+            {
+                await hubConnection.StartAsync();
+            }
+        }
+
+        private async Task SaveAsync()
+        {
+            form.Validate();
+            if (form.IsValid)
+            {
+                var roleRequest = new RoleRequest() { Name = Name, Id = Id };
+                var response = await _roleManager.SaveAsync(roleRequest);
+                if (response.Succeeded)
+                {
+                    _snackBar.Add(localizer[response.Messages[0]], Severity.Success);
+                    await hubConnection.SendAsync(ApplicationConstants.SignalR.SendUpdateDashboard);
+                    MudDialog.Close();
+                }
+                else
+                {
+                    foreach (var message in response.Messages)
+                    {
+                        _snackBar.Add(localizer[message], Severity.Error);
+                    }
+                }
+            }
+        }
+    }
+}
